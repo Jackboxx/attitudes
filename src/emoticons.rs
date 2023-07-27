3dad37ab1015585a8076a903a1686f4d150fd88b
jackboxx <lucygschwantner@gmail.com>
2023-07-27 17:52:25 +0200

    add fuzzy select for emoticons

---
+++ src/emoticons.rs
@@ -0,0 +1,27 @@
+use std::fmt::Display;
+
+pub struct Emoticon<'a> {
+    pub name: &'a str,
+    pub icon: &'a str,
+}
+
+impl<'a> Display for Emoticon<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{name:20}\t {icon}", name = self.name, icon = self.icon)
+    }
+}
+
+pub const DEFAULT_EMOTICONS: [Emoticon; 3] = [
+    Emoticon {
+        name: "Flower Girl",
+        icon: "(◕‿◕✿)",
+    },
+    Emoticon {
+        name: "Cat",
+        icon: "ฅ(^•ﻌ•^ฅ)",
+    },
+    Emoticon {
+        name: "Cat",
+        icon: "(=ʘᆽʘ=)∫",
+    },
+];
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(dead_code)]
+
+mod emoticons;
+
+use dialoguer::console::Term;
+use dialoguer::{theme::ColorfulTheme, FuzzySelect};
+use emoticons::DEFAULT_EMOTICONS;
+
+fn main() -> std::io::Result<()> {
+    let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
+        .items(&DEFAULT_EMOTICONS)
+        .interact_on_opt(&Term::stderr())?;
+
+    let res = selection
+        .map(|idx| DEFAULT_EMOTICONS[idx].icon)
+        .unwrap_or("");
+
+    print!("{res}");
+    Ok(())
 }
