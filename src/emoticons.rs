22d19160e98d50f879e617baaaf89db5bc7931c8
jackboxx <lucygschwantner@gmail.com>
2023-07-27 18:33:45 +0200

    Add copy to clipboard and fallback icon p.s. I regret nothing

--- src/emoticons.rs
+++ src/emoticons.rs
@@ -6,6 +6,10 @@ pub struct Emoticon<'a> {
     pub icon: &'a str,
 }
 
+impl<'a> Emoticon<'a> {
+    pub const FALLBACK_ICON: &str = "¯\\_(ツ)_/¯";
+}
+
 impl<'a> Display for Emoticon<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{name:20}\t {icon}", name = self.name, icon = self.icon)
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@
 mod emoticons;
 
 use clap::Parser;
+use clipboard::{ClipboardContext, ClipboardProvider};
 use dialoguer::console::Term;
 use dialoguer::{theme::ColorfulTheme, FuzzySelect};
 
-use emoticons::DEFAULT_EMOTICONS;
+use emoticons::{Emoticon, DEFAULT_EMOTICONS};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -18,9 +19,13 @@ struct CliArgs {
     /// name use the first icon found
     #[arg(short, long, default_value_t = true)]
     pick_first: bool,
+
+    /// Copy icon to clipboard instead of printing it
+    #[arg(short, long, default_value_t = false)]
+    copy_to_clipboard: bool,
 }
 
-fn main() -> std::io::Result<()> {
+fn main() {
     let args = CliArgs::parse();
 
     let emoticons = DEFAULT_EMOTICONS.to_vec();
@@ -35,17 +40,30 @@ fn main() -> std::io::Result<()> {
     };
 
     let res = if name.is_some() && args.pick_first {
-        possible_choices[0].icon // TODO: don't die if no match
+        possible_choices
+            .get(0)
+            .map(|emo| emo.icon)
+            .unwrap_or(Emoticon::FALLBACK_ICON)
     } else {
         let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
             .items(&possible_choices)
-            .interact_on_opt(&Term::stderr())?;
+            .interact_on_opt(&Term::stderr())
+            .unwrap_or(None);
 
         selection
             .map(|idx| possible_choices[idx].icon)
-            .unwrap_or("")
+            .unwrap_or(Emoticon::FALLBACK_ICON)
     };
 
-    print!("{res}");
-    Ok(())
+    // ¯\_(ツ)_/¯
+    if !args.copy_to_clipboard
+        || (|| {
+            let mut ctx: ClipboardContext = ClipboardProvider::new()?;
+            ctx.set_contents(res.to_owned())?;
+            Ok::<(), Box<dyn std::error::Error>>(())
+        })()
+        .is_err()
+    {
+        print!("{res}");
+    }
 }
